6fe0c87728295206ceea6cde74ae3ade1abff0e0
cicero884 <cicero884@gmail.com>
2019-04-13 16:12:15 +0800

    Create gitignore and remove usless function

--- ehentai/request.js
+++ ehentai/request.js
@@ -3,22 +3,12 @@ const jsdom=require('jsdom'),
 	request=require('request');
 const { JSDOM } = jsdom;
 
-function downloadImage(item,index){
-	jsdom.env(item.href,
-		["http://code.jquery.com/jquery-1.7.1.min.js"],function(err,window){
-			url=$('#img').src;
-			request.head(uri, function(err, res, body){
-				request(uri).pipe(fs.createWriteStream(dir+index+'.jpg')).on('close', callback);
-			});
-		})
-}
-
 function getImage(website){ 
 	request(website, function(error, response, body) {
 		let {document}=(new JSDOM(body)).window;
 		let array=document.getElementById('gdt').getElementsByTagName('a');
 
-		let dir = './'+document.getElementById('gn').innerHTML+'/';
+		let dir = './output/'+document.getElementById('gn').innerHTML+'/';
 		if (!fs.existsSync(dir)) fs.mkdirSync(dir);
 
 		for(let i=0;i<array.length;++i){
