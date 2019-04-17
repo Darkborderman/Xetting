952365a982bae285ace0c30f885b3f1a759b3922
hangalice2156 <hangalice2156@gmail.com>
2019-04-17 20:15:48 +0800

    Done basic requirements
    For details, refer to README.md
    most of the code is modified with Darkborderman's nhentai downloader.

--- doujinantena/main.js
+++ doujinantena/main.js
@@ -3,10 +3,6 @@ const { JSDOM } = jsdom;
 const request=require('request');
 const fs=require('fs');
 
-//get html argument
-//if not enough just return
-//uh.. I write a monster
-
 if(process.argv.length<=2){
     console.log("not enough arguments");
     return;
@@ -16,8 +12,7 @@ else{
     let web='doujinantena';
     let bookNumber=process.argv[2];
     let dir=createDir(web,bookNumber);
-    //let page=getPages(`https://nhentai.net/g/${bookNumber}/`);
-    let page=getPages(`http://doujinantena.com/page.php?id=${bookNumber}`);
+    let page=getPages(`http://doujinantena.com/page.php?id=${bookNumber}&p=1`);
 
     page.then(function(resolve,reject){
         console.log(resolve);
@@ -49,17 +44,10 @@ function getPages(uri){
             let rawString=pagesElement[0].nextElementSibling.innerHTML;
             let filterPatt=/[(/)]/g;
             let pagesString = rawString.split(filterPatt);
-            console.log(pagesString);
             let pages=parseInt(pagesString[2]);
-            
-            /*let galleryElement=document.getElementsByClassName("gallerythumb")[2].childNodes[1].attributes["data-src"].value;
-            let test=galleryElement.split('galleries/')[1];
-            test=test.split('/')[0];
-            let galleryNumber=parseInt(test);*/
 
-            let galleryElement = pagesElement[0].nextElementSibling.nextElementSibling.childNodes[1].attributes["href"].value;
-            let galleryDirectories = galleryElement.split('/');
-            let galleryNumber = galleryDirectories[4];
+            let  imagesource=document.getElementsByTagName('img')[0].getAttribute('src');
+            let galleryNumber = imagesource.split('/')[4];
 
             let filetype = '.jpg';
 
@@ -69,15 +57,6 @@ function getPages(uri){
                 pageNumber:pages,
                 filetype:filetype,
             };
-
-            /*let galleryElementArray=document.getElementsByClassName("gallerythumb");
-            
-            for(let i=0;i<=pages-1;i++)
-            {
-                let element=galleryElementArray[i].childNodes[1].attributes["data-src"].value;
-                element.split('.');
-                Package.filetype[i]=element.split('.')[3];
-            }*/
 
             resolve(Package);
         });
@@ -90,11 +69,11 @@ function downloadImage(number,pages,type,targetDir){
     {
         //need to adjust array iterate
         //i from 1-25, type from 0-24
-        //let uri=`https://i.nhentai.net/galleries/${number}/${i}.${type[i-1]}`;
         let page = i.toString().padStart('3','0');
-        let uri=`http://cdn.doujinantena.com/contents_jpg/${number}/${page}.${type}`;
+        let uri=`http://cdn.doujinantena.com/contents_jpg/${number}/${page}${type}`;
+        console.log(uri)
         request(uri).pipe(fs.createWriteStream(`${targetDir}/${page}.jpg`)).on('close',function(){
-            console.log(page + 'done');
+
         });
     }
 }
