752231ba1b42eee41d94b3382fcb26eba347f6a6
cicero884 <joy861225@gmail.com>
2019-05-30 20:41:18 +0800

    Add nhentai search module

--- modules/nhentai.js
+++ modules/nhentai.js
@@ -1,43 +1,55 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-const request = require('request');
-//const { window } = new JSDOM();
-//const { document } = (new JSDOM('')).window;
-//global.document = document;
-const {nhentaiPageListMax} = 25;
 
-let result=[];
-async function search(input,begin,end){
-	result=[];
+const request = require('request');
+
+const nhentaiPageListMax = 25;
+
+function search(input,begin,end){
+	let result=[];
 	input.split(' ').join('+');
-	beginPage=begin/nhentaiPageListMax+1;
-	endPage=end/nhentaiPageListMax+1;
-	begin%=nhentaiPageListMax;
-	end%=nhentaiPageListMax;
-	let url="https://nhentai.net/search/?q="+encodeURI(input);
-	if(beginPage==endPage) search_page(url,beginPage,begin,end);
-	else{
-		await search_page(url,beginPage,begin,nhentaiPageListMax-1);
-		for(let i=begin+1;i<endPage;++i)
-			await search_page(url,i,0,nhentaiPageListMax-1);
-		await search_page(url,endPage,0,end);
-	}
-}
-async function search_page(url,page,begin,end){
-	url=url+"&page="+page;
-	request(url,function(err,res,body){
-		let window = new JSDOM(body);
-		let $ = jQuery = require('jquery')(window);
-		for(let i=begin;i<=end;++i){
-			let g=$('.index-container').children[i].children[0];
-			result.push({
-				"source":"nhentai",
-				"booknumber":g.pathname.split("/")[2],
-				"thumbnail":g.firstChild.src,
-				"title":g.lastChild.textContent
-			});
+	return new Promise(async (resolve, reject) => {
+		let beginPage=Math.floor(begin/nhentaiPageListMax+1),
+			endPage=Math.floor(end/nhentaiPageListMax+1);
+		begin%=nhentaiPageListMax;
+		end%=nhentaiPageListMax;
+		let url="https://nhentai.net/search/?q="+encodeURI(input);
+		if(beginPage==endPage) await search_page(url,beginPage,begin,end);
+		else{
+			result=result.concat(await search_page(url,beginPage,begin,nhentaiPageListMax-1));
+			for(let i=beginPage+1;i<endPage;++i)
+				result=result.concat(await search_page(url,i,0,nhentaiPageListMax-1));
+			result=result.concat(await search_page(url,endPage,0,end));
 		}
+		resolve(result);
 	});
-	
 }
-console.log(search("test",20,50));
+
+function search_page(url,page,begin,end){
+	url=url+"&page="+page;
+	return new Promise((resolve, reject) => {
+		request(url,function(err,res,body){
+			let result=[];
+			let {window} = new JSDOM(body);
+			let document = window.window;
+			let $ = jQuery = require('jquery')(window);
+
+			for(let i=begin;i<=end;++i){
+
+				let g=$('.index-container')[0].children[i].children[0];
+				result.push({
+					"source":"nhentai",
+					"booknumber":g.href.split("/")[2],
+					"thumbnail":g.firstChild.src,
+					"title":g.lastChild.textContent
+				});
+			}
+			resolve(result);
+		});
+	});
+}
+/*
+(async function (){
+	console.log(await search("test",20,50));
+}());
+*/
