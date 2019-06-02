023ff5cf3125e5558c15dae53d6af24e7f1a1d40
cicero884 <joy861225@gmail.com>
2019-06-02 18:10:32 +0800

    Fix gitignore .swp file,add nhentai search

--- modules/crawlerController.js
+++ modules/crawlerController.js
@@ -1,10 +1,16 @@
 const nhentai=require(`./nhentai.js`);
+const ehentai=require(`./ehentai.js`);
 
 const Crawler={
 	nhentai:{
 		search:nhentai.search,
 		bookDetail:nhentai.getBook,
 		downloadBook:nhentai.downloadBook
+	},
+	ehentai:{
+		search:ehentai.search,
+		bookDetail:ehentai.getBook,
+		downloadBook:ehentai.downloadBook
 	}
 };
 
@@ -13,7 +19,7 @@ const Crawler={
 /*
 (async function (){
 	//console.log(await search("test",20,50));
-	console.log(await Crawler.nhentai.downloadBook(273908));
+	console.log(await Crawler.ehentai.search('lol',0,5));
 }());
 search:function(input,begin,end,option){
 	return new Promise(async (resolve, reject) => {
